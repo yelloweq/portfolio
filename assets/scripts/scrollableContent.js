383c1caf0e40948c29de5862433a218824b30cd2
yelloweq <iwo.grzes237@gmail.com>
2024-08-23 20:36:10 +0100

    Project link, active route indication, disable background, lint

--- assets/scripts/scrollableContent.js
+++ assets/scripts/scrollableContent.js
@@ -1,12 +1,23 @@
-document.addEventListener("wheel", function (e) {
-
-  const scrollableContent = document.getElementById('scrollableContent');
-  const maxNegativeTranslateY = Math.min(window.innerHeight - scrollableContent.clientHeight, 0);
-  const oldVal = parseFloat(scrollableContent.style.transform.replace(/translateY\((-?\d+)px\)/, '$1')) || 0;
-  let newVal = oldVal - e.deltaY;
-  newVal = Math.max(newVal, maxNegativeTranslateY);
-  newVal = Math.min(newVal, 0);
-  scrollableContent.style.transform = `translateY(${newVal}px)`;
-  e.preventDefault();
-}, { passive: false });
-
+document.addEventListener(
+	"wheel",
+	(e) => {
+		const scrollableContent = document.getElementById("scrollableContent");
+		const maxNegativeTranslateY = Math.min(
+			window.innerHeight - scrollableContent.clientHeight,
+			0,
+		);
+		const oldVal =
+			Number.parseFloat(
+				scrollableContent.style.transform.replace(
+					/translateY\((-?\d+)px\)/,
+					"$1",
+				),
+			) || 0;
+		let newVal = oldVal - e.deltaY;
+		newVal = Math.max(newVal, maxNegativeTranslateY);
+		newVal = Math.min(newVal, 0);
+		scrollableContent.style.transform = `translateY(${newVal}px)`;
+		e.preventDefault();
+	},
+	{ passive: false },
+);
